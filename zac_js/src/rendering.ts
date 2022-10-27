5b28b617107072248e23bcf17278c3bc67e482eb
Michael <smillgit@gmail.com>
2022-10-27 19:36:34 +0200

    added rendering, services

---
+++ zac_js/src/rendering.ts
@@ -0,0 +1,227 @@
+class HitTestBehavior extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+
+    static deferToChild() {
+        return new HitTestBehavior({ converter: 'f:1:HitTestBehavior.deferToChild' })
+    }
+
+    static opaque() {
+        return new HitTestBehavior({ converter: 'f:1:HitTestBehavior.opaque' })
+    }
+
+    static translucent() {
+        return new HitTestBehavior({ converter: 'f:1:HitTestBehavior.translucent' })
+    }
+}
+
+class CrossAxisAlignment extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+
+    static baseline() {
+        return new CrossAxisAlignment({ converter: 'f:1:CrossAxisAlignment.baseline' })
+    }
+
+    static center() {
+        return new CrossAxisAlignment({ converter: 'f:1:CrossAxisAlignment.center' })
+    }
+
+    static end() {
+        return new CrossAxisAlignment({ converter: 'f:1:CrossAxisAlignment.end' })
+    }
+
+    static start() {
+        return new CrossAxisAlignment({ converter: 'f:1:CrossAxisAlignment.start' })
+    }
+
+    static stretch() {
+        return new CrossAxisAlignment({ converter: 'f:1:CrossAxisAlignment.stretch' })
+    }
+}
+
+class MainAxisAlignment extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+
+    static start() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.start' })
+    }
+
+    static end() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.end' })
+    }
+
+    static center() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.center' })
+    }
+
+    static spaceBetween() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.spaceBetween' })
+    }
+
+    static spaceAround() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.spaceAround' })
+    }
+
+    static spaceEvenly() {
+        return new MainAxisAlignment({ converter: 'f:1:MainAxisAlignment.spaceEvenly' })
+    }
+}
+
+class MainAxisSize extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+
+    static min() {
+        return new MainAxisSize({ converter: 'f:1:MainAxisSize.min' })
+    }
+
+    static max() {
+        return new MainAxisSize({ converter: 'f:1:MainAxisSize.max' })
+    }
+}
+
+class FlexFit extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static tight() {
+        return new FlexFit({ converter: 'f:1:FlexFit.tight' })
+    }
+    static loose() {
+        return new FlexFit({ converter: 'f:1:FlexFit.loose' })
+    }
+}
+
+class WrapAlignment extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static start() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.start' })
+    }
+    static end() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.end' })
+    }
+    static center() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.center' })
+    }
+    static spaceBetween() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.spaceBetween' })
+    }
+    static spaceAround() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.spaceAround' })
+    }
+    static spaceEvenly() {
+        return new WrapAlignment({ converter: 'f:1:WrapAlignment.spaceEvenly' })
+    }
+}
+class WrapCrossAlignment extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static center() {
+        return new WrapCrossAlignment({ converter: 'f:1:WrapCrossAlignment.center' })
+    }
+    static end() {
+        return new WrapCrossAlignment({ converter: 'f:1:WrapCrossAlignment.end' })
+    }
+    static start() {
+        return new WrapCrossAlignment({ converter: 'f:1:WrapCrossAlignment.start' })
+    }
+}
+class StackFit extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static expand() {
+        return new StackFit({ converter: 'f:1:StackFit.expand' })
+    }
+    static loose() {
+        return new StackFit({ converter: 'f:1:StackFit.loose' })
+    }
+    static passthrough() {
+        return new StackFit({ converter: 'f:1:StackFit.passthrough' })
+    }
+}
+class BoxConstraints extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static new(data: {
+        minWidth?: ZacValue<DartDouble>,
+        maxWidth?: ZacValue<DartDouble>,
+        minHeight?: ZacValue<DartDouble>,
+        maxHeight?: ZacValue<DartDouble>,
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints', ...data })
+    }
+    static expand(data: {
+        width?: ZacValue<DartDouble>
+        height?: ZacValue<DartDouble>
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints.expand', ...data })
+    }
+    static loose(data: {
+        size: Size
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints.loose', ...data })
+    }
+    static tight(data: {
+        size: Size
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints.tight', ...data })
+    }
+    static tightFor(data: {
+        width?: ZacValue<DartDouble>,
+        height?: ZacValue<DartDouble>
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints.tightFor', ...data })
+    }
+    static tightForFinite(data: {
+        width?: ZacValue<DartDouble>,
+        height?: ZacValue<DartDouble>
+    }) {
+        return new BoxConstraints({ converter: 'f:1:BoxConstraints.tightForFinite', ...data })
+    }
+}
+class SliverGridDelegate extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static fixedCrossAxisCount(data: {
+        crossAxisCount: DartInt,
+        mainAxisSpacing?: ZacValue<DartDouble>,
+        crossAxisSpacing?: ZacValue<DartDouble>,
+        childAspectRatio?: ZacValue<DartDouble>,
+        mainAxisExtent?: ZacValue<DartDouble>
+    }) {
+        return new SliverGridDelegate({ converter: 'f:1:SliverGridDelegateWithFixedCrossAxisCount', ...data })
+    }
+
+    static maxCrossAxisExtent(data: {
+        maxCrossAxisExtent: ZacValue<DartDouble>,
+        mainAxisSpacing?: ZacValue<DartDouble>,
+        crossAxisSpacing?: ZacValue<DartDouble>,
+        childAspectRatio?: ZacValue<DartDouble>,
+        mainAxisExtent?: ZacValue<DartDouble>
+    }) {
+        return new SliverGridDelegate({ converter: 'f:1:SliverGridDelegateWithMaxCrossAxisExtent', ...data })
+    }
+}
+class DecorationPosition extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static background() {
+        return new DecorationPosition({ converter: 'f:1:DecorationPosition.background' })
+    }
+    static foreground() {
+        return new DecorationPosition({ converter: 'f:1:DecorationPosition.foreground' })
+    }
+}
---
+++ zac_js/src/services.ts
@@ -0,0 +1,138 @@
+class SystemUiOverlayStyle extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static new(data: {
+        systemNavigationBarColor?: Color,
+        systemNavigationBarDividerColor?: Color,
+        systemNavigationBarIconBrightness?: Brightness,
+        systemNavigationBarContrastEnforced?: ZacValue<boolean>,
+        statusBarColor?: Color,
+        statusBarBrightness?: Brightness,
+        statusBarIconBrightness?: Brightness,
+        systemStatusBarContrastEnforced?: ZacValue<boolean>
+    }) {
+        return new SystemUiOverlayStyle({ converter: 'f:1:SystemUiOverlayStyle', ...data })
+    }
+}
+class TextInputType extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static datetime() {
+        return new TextInputType({ converter: 'f:1:TextInputType.datetime' })
+    }
+    static emailAddress() {
+        return new TextInputType({ converter: 'f:1:TextInputType.emailAddress' })
+    }
+    static multiline() {
+        return new TextInputType({ converter: 'f:1:TextInputType.multiline' })
+    }
+    static name_() {
+        return new TextInputType({ converter: 'f:1:TextInputType.name' })
+    }
+    static none() {
+        return new TextInputType({ converter: 'f:1:TextInputType.none' })
+    }
+    static number() {
+        return new TextInputType({ converter: 'f:1:TextInputType.number' })
+    }
+    static phone() {
+        return new TextInputType({ converter: 'f:1:TextInputType.phone' })
+    }
+    static streetAddress() {
+        return new TextInputType({ converter: 'f:1:TextInputType.streetAddress' })
+    }
+    static text() {
+        return new TextInputType({ converter: 'f:1:TextInputType.text' })
+    }
+    static url() {
+        return new TextInputType({ converter: 'f:1:TextInputType.url' })
+    }
+    static visiblePassword() {
+        return new TextInputType({ converter: 'f:1:TextInputType.visiblePassword' })
+    }
+}
+class TextInputAction extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static continueAction() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.continueAction' })
+    }
+    static done() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.done' })
+    }
+    static emergencyCall() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.emergencyCall' })
+    }
+    static go() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.go' })
+    }
+    static join() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.join' })
+    }
+    static newline() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.newline' })
+    }
+    static next() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.next' })
+    }
+    static none() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.none' })
+    }
+    static previous() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.previous' })
+    }
+    static route() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.route' })
+    }
+    static search() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.search' })
+    }
+    static send() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.send' })
+    }
+    static unspecified() {
+        return new TextInputAction({ converter: 'f:1:TextInputAction.unspecified' })
+    }
+}
+class TextCapitalization extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static characters() {
+        return new TextCapitalization({ converter: 'f:1:TextCapitalization.characters' })
+    }
+    static none() {
+        return new TextCapitalization({ converter: 'f:1:TextCapitalization.none' })
+    }
+    static sentences() {
+        return new TextCapitalization({ converter: 'f:1:TextCapitalization.sentences' })
+    }
+    static words() {
+        return new TextCapitalization({ converter: 'f:1:TextCapitalization.words' })
+    }
+}
+class SmartDashesType extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static disabled() {
+        return new SmartDashesType({ converter: 'f:1:SmartDashesType.disabled' })
+    }
+    static enabled() {
+        return new SmartDashesType({ converter: 'f:1:SmartDashesType.enabled' })
+    }
+}
+class SmartQuotesType extends ZacConverter {
+    private constructor(data: ZacConverterType) {
+        super(data);
+    }
+    static disabled() {
+        return new SmartQuotesType({ converter: 'f:1:SmartQuotesType.disabled' })
+    }
+    static enabled() {
+        return new SmartQuotesType({ converter: 'f:1:SmartQuotesType.enabled' })
+    }
+}
