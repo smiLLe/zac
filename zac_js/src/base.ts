d45653754de17570d4c02b9a567d90d31efcbca3
Michael <smillgit@gmail.com>
2022-10-28 11:59:56 +0200

    added multi children layouts; added ZacValueList

--- zac_js/src/base.ts
+++ zac_js/src/base.ts
@@ -34,13 +34,15 @@ export class ZacValue<T> extends ZacConverter {
     // ZacValue<int> to ZacValue<string> will result in an error.
     private ignoredProp: T | undefined;
 
-    static new<T extends ValidTypes>(data: T) {
-        return new ZacValue<T>({
+    static new<T extends ValidTypes>(data: {
+        value: T,
+        transformer?: ZacTransformers,
+    }) {
+        return new ZacValueList<T>({
             converter: 'z:1:ZacValue',
-            value: data,
+            ...data,
         });
     }
-
     static watch(data: {
         family: SharedValueFamily
     }) {
@@ -56,6 +58,56 @@ export class ZacValue<T> extends ZacConverter {
         return new ZacValue({
             converter: 'z:1:ZacValue.read',
             ...data,
+        });
+    }
+}
+
+export class ZacValueList<T> extends ZacConverter {
+    // The generic has to be used in some way so that assigning
+    // ZacValue<int> to ZacValue<string> will result in an error.
+    private ignoredProp: T | undefined;
+
+    static new<T extends ValidTypes>(data: {
+        value: Array<ZacValue<T>>,
+        transformer?: ZacTransformers,
+    }) {
+        return new ZacValueList<T>({
+            converter: 'z:1:ZacValue',
+            ...data,
+        });
+    }
+
+    static watch(data: {
+        family: SharedValueFamily,
+        transformer?: ZacTransformers,
+        select?: ZacTransformers,
+    }) {
+        return new ZacValueList({
+            converter: 'z:1:ZacValue.watch',
+            ...data,
+        });
+    }
+
+    static read(data: {
+        family: SharedValueFamily,
+        transformer?: ZacTransformers
+    }) {
+        return new ZacValueList({
+            converter: 'z:1:ZacValue.read',
+            ...data,
+        });
+    }
+}
+
+export abstract class ZacTransformer extends ZacConverter { }
+
+export class ZacTransformers extends ZacConverter {
+    static new(data: {
+        transformers: Array<ZacTransformer>
+    }) {
+        return new ZacTransformers({
+            converter: 'z:1:ZacValue',
+            ...data
         });
     }
 }
