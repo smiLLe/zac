aed2e51b0248f7ac0ccaa2825e329f56362a0524
Michael <smillgit@gmail.com>
2022-11-06 10:47:01 +0100

    better refresh_indicator completer handling

--- zac/packages/zac/lib/src/flutter/widgets/material/refresh_indicator.dart
+++ zac/packages/zac/lib/src/flutter/widgets/material/refresh_indicator.dart
@@ -49,40 +49,36 @@ class FlutterRefreshIndicator
 
   @override
   Widget buildWidget(ZacContext zacContext) {
-    return ProviderScope(
+    return SharedValueProvider(
       key: key?.buildKey(zacContext),
-      overrides: [
-        SharedValue.provider(family).overrideWithProvider(
-            AutoDisposeStateProvider<SharedValueType>((ref) {
-          final completer = Completer<void>();
-          ref.onDispose(() {
-            if (completer.isCompleted) return;
-            completer.complete();
-          });
-          return completer;
-        })),
-      ],
-      child: ZacUpdateContext(
-        builder: (zacContext) {
+      autoCreate: true,
+      family: family,
+      valueBuilder: (ref, zacContext) {
+        final completer = Completer<void>();
+        ref.onDispose(() {
+          if (completer.isCompleted) return;
+          completer.complete();
+        });
+        return completer;
+      },
+      childBuilder: (zacContext) => RefreshIndicator(
+        onRefresh: () async {
+          zacContext.ref.invalidate(SharedValue.provider(family));
           final completer = SharedValue.get(
-                  const SharedValueConsumeType.watch(), zacContext, family)
+                  const SharedValueConsumeType.read(), zacContext, family)
               as Completer<void>;
-          return RefreshIndicator(
-            onRefresh: () async {
-              onRefresh.execute(ZacActionPayload.param(completer), zacContext);
-              return completer.future;
-            },
-            displacement: displacement?.getValue(zacContext) ?? 40.0,
-            edgeOffset: edgeOffset?.getValue(zacContext) ?? 0.0,
-            color: color?.build(zacContext),
-            backgroundColor: backgroundColor?.build(zacContext),
-            semanticsLabel: semanticsLabel?.getValue(zacContext),
-            semanticsValue: semanticsValue?.getValue(zacContext),
-            triggerMode: triggerMode?.build(zacContext) ??
-                RefreshIndicatorTriggerMode.onEdge,
-            child: child.buildWidget(zacContext),
-          );
+          onRefresh.execute(ZacActionPayload.param(completer), zacContext);
+          return completer.future;
         },
+        displacement: displacement?.getValue(zacContext) ?? 40.0,
+        edgeOffset: edgeOffset?.getValue(zacContext) ?? 0.0,
+        color: color?.build(zacContext),
+        backgroundColor: backgroundColor?.build(zacContext),
+        semanticsLabel: semanticsLabel?.getValue(zacContext),
+        semanticsValue: semanticsValue?.getValue(zacContext),
+        triggerMode: triggerMode?.build(zacContext) ??
+            RefreshIndicatorTriggerMode.onEdge,
+        child: child.buildWidget(zacContext),
       ),
     );
   }
